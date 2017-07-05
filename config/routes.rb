383014394701d459159557a9df725fdf99bb6986
Worlanyo <worlalidamalie@gmail.com>
2017-07-05 16:08:32 +0000

    Add html views to backends
    Add views to email,dashboard,landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,25 @@
 Rails.application.routes.draw do
 
-  get 'static_pages/home'
+  # get 'static_pages/home'
 
-  get 'organisations/index'
+  # get 'organisations/index'
 
    devise_for :organisations, :controllers => { registrations: 'registrations' }
   # # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
+
   get '/home', to: 'static_pages#home'
+
+  resources :organisations do
+    collection do
+       get :email
+       get :event
+    end
+  end
+
+  devise_scope :organisation do
+     get '/dashboard', to: 'organisations#index'
+  end
+
+
 end
